c16b6a1d76dfcc00ff8c4564e2ca25bef6e757da
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-03 14:37:33 +0300

    Controller created with CRUD Methods

---
+++ WebAPI/Controllers/ProgrammingLanguagesController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Features.ProgrammingLanguages.Commands;
+using Application.Features.ProgrammingLanguages.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class ProgrammingLanguagesController : BaseController
+    {
+        [HttpPost("add")]
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+        {
+            CreatedProgrammingLanguageDto result = await Mediator.Send(createProgrammingLanguageCommand);
+            return Created("", result);
+        }
+
+        [HttpPost("update")]
+        public async Task<IActionResult> Update([FromBody] UpdateProgrammingLanguageCommand updateProgrammingLanguageCommand)
+        {
+            UpdatedProgrammingLanguageDto result = await Mediator.Send(updateProgrammingLanguageCommand);
+            return Ok(result);
+        }
+
+        [HttpPost("delete")]
+        public async Task<IActionResult> Delete([FromBody] DeleteProgrammingLanguageCommand deleteProgrammingLanguageCommand)
+        {
+            DeletedProgrammingLanguageDto result = await Mediator.Send(deleteProgrammingLanguageCommand);
+            return Ok(result);
+        }
+
+
+    }
+}
