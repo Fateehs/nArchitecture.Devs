1bed1bdd2676fab6a807d278c4eef80ffd7c8775
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-04 13:27:01 +0300

    Select Operation Query added

---
+++ Application/Features/ProgrammingLanguages/Queries/GetByIdProgrammingLanguage/GetByIdProgrammingLanguageQuery.cs
@@ -0,0 +1,41 @@
+﻿using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Services;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Queries.GetByIdProgrammingLanguage
+{
+    public class GetByIdProgrammingLanguageQuery : IRequest<ProgrammingLanguageGetByIdDto>
+    {
+        public int Id { get; set; }
+
+        public class GetByIdProgrammingLanguageQueryHandler : IRequestHandler<GetByIdProgrammingLanguageQuery, ProgrammingLanguageGetByIdDto>
+        {
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+            private readonly IMapper _mapper;
+
+            public GetByIdProgrammingLanguageQueryHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper)
+            {
+                _programmingLanguageRepository = programmingLanguageRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<ProgrammingLanguageGetByIdDto> Handle(GetByIdProgrammingLanguageQuery request, CancellationToken cancellationToken)
+            {
+                ProgrammingLanguage programmingLanguage = await _programmingLanguageRepository.GetAsync(b => b.Id == request.Id);
+
+                ProgrammingLanguageGetByIdDto programmingLanguageGetByIdDto = _mapper.Map<ProgrammingLanguageGetByIdDto>(programmingLanguage);
+
+                return programmingLanguageGetByIdDto;
+            }
+        }
+
+
+    }
+}
