ea8925dbaf1623654318add01ec6f94e3da82c9a
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-04 15:09:35 +0300

    Validator created @FluentValidation

---
+++ Application/Features/ProgrammingLanguages/Commands/Update/UpdateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.ProgrammingLanguages.Commands.Create;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Commands.Update
+{
+    public class UpdateProgrammingLanguageCommandValidator : AbstractValidator<UpdateProgrammingLanguageCommand>
+    {
+        public UpdateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.Name).MinimumLength(1);
+        }
+    }
+}
