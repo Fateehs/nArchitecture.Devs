7f4c48bae034d96ce74363dc7e8361a3202a45f3
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-06 20:24:18 +0300

    New Entity linked

--- Persistence/Contexts/BaseDbContext.cs
+++ Persistence/Contexts/BaseDbContext.cs
@@ -13,10 +13,11 @@ namespace Persistence.Contexts
     {
         protected IConfiguration Configuration { get; set; }
         public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+        public DbSet<SubTechnology> SubTechnologies { get; set; }
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         {
-            Configuration = configuration; 
+            Configuration = configuration;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -24,12 +25,32 @@ namespace Persistence.Contexts
             modelBuilder.Entity<ProgrammingLanguage>(a =>
             {
                 a.ToTable("ProgrammingLanguages").HasKey(k => k.Id);
+
                 a.Property(p => p.Id).HasColumnName("Id");
                 a.Property(p => p.Name).HasColumnName("Name");
+
+                a.HasMany(p => p.SubTechnologies);
             });
 
-            ProgrammingLanguage[] programmingLanguageEntitySeeds = { new(1, "C#"), new(2, "Java"), new(3, "Python") };
+            ProgrammingLanguage[] programmingLanguageEntitySeeds =
+                { new(1, "C#"), new(2, "Java"), new(3, "Python"), new(4, "JavaScript") };
             modelBuilder.Entity<ProgrammingLanguage>().HasData(programmingLanguageEntitySeeds);
+
+            modelBuilder.Entity<SubTechnology>(a =>
+            {
+                a.ToTable("SubTechnologies").HasKey(k => k.Id);
+
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.ProgrammingLanguageId).HasColumnName("ProgrammingLanguageId");
+                a.Property(p => p.Name).HasColumnName("Name");
+
+                a.HasOne(p => p.ProgrammingLanguage);
+            });
+
+            SubTechnology[] subTechnologyEntitySeeds =
+                { new(1, 1, "WPF"), new(2, 1, "ASP.NET"), new(3, 2, "Spring"), new(4, 2, "JSP"), new(5, 4, "Vue"), new(6, 4, "React") };
+            modelBuilder.Entity<SubTechnology>().HasData(subTechnologyEntitySeeds);
+
         }
 
     }
