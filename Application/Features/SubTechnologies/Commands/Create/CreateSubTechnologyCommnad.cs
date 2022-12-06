3b556981531410de3971495c9cde1bd54833a5a6
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-06 20:10:35 +0300

    CRUD Operation Command added

---
+++ Application/Features/SubTechnologies/Commands/Create/CreateSubTechnologyCommnad.cs
@@ -0,0 +1,42 @@
+﻿using Application.Features.SubTechnologies.Dtos;
+using Application.Services;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.SubTechnologies.Commands.Create
+{
+    public class CreateSubTechnologyCommand : IRequest<CreatedSubTechnologyDto>
+    {
+        public int ProgrammingLanguageId { get; set; }
+        public string Name { get; set; }
+
+        public class CreateSubTechnologyCommandHandler : IRequestHandler<CreateSubTechnologyCommand, CreatedSubTechnologyDto>
+        {
+            private readonly ISubTechnologyRepository _subTechnologyRepository;
+            private readonly IMapper _mapper;
+
+            public CreateSubTechnologyCommandHandler(ISubTechnologyRepository subTechnologyRepository, IMapper mapper)
+            {
+                _subTechnologyRepository = subTechnologyRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<CreatedSubTechnologyDto> Handle(CreateSubTechnologyCommand request, CancellationToken cancellationToken)
+            {
+                SubTechnology mappedSubTechnology = _mapper.Map<SubTechnology>(request);
+                SubTechnology createSubTechnology = await _subTechnologyRepository.AddAsync(mappedSubTechnology);
+
+                CreatedSubTechnologyDto createdSubTechnologyDto = _mapper.Map<CreatedSubTechnologyDto>(createSubTechnology);
+
+                return createdSubTechnologyDto;
+            }
+        }
+
+    }
+}
---
+++ Application/Features/SubTechnologies/Commands/Delete/DeleteSubTechnologyCommand.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.SubTechnologies.Dtos;
+using Application.Services;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.SubTechnologies.Commands.Delete
+{
+    public class DeleteSubTechnologyCommand : IRequest<DeletedSubTechnologyDto>
+    {
+        public int Id { get; set; }
+
+        public class DeleteSubTechnologyCommandHandler : IRequestHandler<DeleteSubTechnologyCommand, DeletedSubTechnologyDto>
+        {
+            private readonly ISubTechnologyRepository _subTechnologyRepository;
+            private readonly IMapper _mapper;
+
+            public DeleteSubTechnologyCommandHandler(ISubTechnologyRepository subTechnologyRepository, IMapper mapper)
+            {
+                _subTechnologyRepository = subTechnologyRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<DeletedSubTechnologyDto> Handle(DeleteSubTechnologyCommand request, CancellationToken cancellationToken)
+            {
+                SubTechnology mappedSubTechnology = _mapper.Map<SubTechnology>(request);
+                SubTechnology deletedSubTechnology = await _subTechnologyRepository.DeleteAsync(mappedSubTechnology);
+
+                DeletedSubTechnologyDto deletedSubTechnologyDto = _mapper.Map<DeletedSubTechnologyDto>(deletedSubTechnology);
+
+                return deletedSubTechnologyDto;
+            }
+        }
+    }
+}
