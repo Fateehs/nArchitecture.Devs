7abe9752c5f47f11cb7daf2e142a90ac467ed742
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-07 19:15:28 +0300

    Register Command added

---
+++ Application/Features/Auths/Commands/RegisterCommand.cs
@@ -0,0 +1,68 @@
+﻿using Application.Features.Auths.Dtos;
+using Application.Features.Auths.Rules;
+using Application.Services.Auth;
+using Application.Services.Repositories;
+using Core.Security.Dtos;
+using Core.Security.Entities;
+using Core.Security.Hashing;
+using Core.Security.JWT;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auths.Commands
+{
+    public class RegisterCommand : IRequest<RegisteredDto>
+    {
+        public UserForRegisterDto UserForRegisterDto { get; set; }
+        public string IpAddress { get; set; }
+
+        public class RegisterCommandHandler : IRequestHandler<RegisterCommand, RegisteredDto>
+        {
+            private readonly IUserRepository _userRepository;
+            private readonly IAuthService _authService;
+            private readonly AuthBusinessRules _authBusinessRules;
+
+            public RegisterCommandHandler(IUserRepository userRepository, IAuthService authService, AuthBusinessRules authBusinessRules)
+            {
+                _userRepository = userRepository;
+                _authService = authService;
+                _authBusinessRules = authBusinessRules;
+            }
+
+            public async Task<RegisteredDto> Handle(RegisterCommand request, CancellationToken cancellationToken)
+            {
+                await _authBusinessRules.EmailCanNotBeDuplicatedWhenRegistered(request.UserForRegisterDto.Email);
+                byte[] passwordHash, passwordSalt;
+                HashingHelper.CreatePasswordHash(request.UserForRegisterDto.Password, out passwordHash, out passwordSalt);
+
+                User newUser = new()
+                {
+                    Email = request.UserForRegisterDto.Email,
+                    PasswordHash = passwordHash,
+                    PasswordSalt = passwordSalt,
+                    FirstName = request.UserForRegisterDto.FirstName,
+                    LastName = request.UserForRegisterDto.LastName,
+                    Status = true
+                };
+
+                User createdUser = await _userRepository.AddAsync(newUser);
+
+                AccessToken createdAccessToken = await _authService.CreateAccessToken(createdUser);
+                RefreshToken createdRefreshToken = await _authService.CreateRefreshToken(createdUser, request.IpAddress);
+                RefreshToken addedRefreshToken = await _authService.AddRefreshToken(createdRefreshToken);
+
+                RegisteredDto registeredDto = new()
+                {
+                    RefreshToken = addedRefreshToken,
+                    AccessToken = createdAccessToken,
+                };
+                return registeredDto;
+
+            }
+        }
+    }
+}
