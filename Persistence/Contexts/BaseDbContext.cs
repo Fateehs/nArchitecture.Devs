fe1c5660c3bc0d4cb17b3764177590032b5e9885
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-07 19:30:43 +0300

    Db Set Configuration added

--- Persistence/Contexts/BaseDbContext.cs
+++ Persistence/Contexts/BaseDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Entities;
+﻿using Core.Security.Entities;
+using Domain.Entities;
+using Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -13,7 +15,11 @@ namespace Persistence.Contexts
     {
         protected IConfiguration Configuration { get; set; }
         public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
-        public DbSet<SubTechnology> SubTechnologies { get; set; }
+        public DbSet<Technology> Technologies { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         {
@@ -29,16 +35,16 @@ namespace Persistence.Contexts
                 a.Property(p => p.Id).HasColumnName("Id");
                 a.Property(p => p.Name).HasColumnName("Name");
 
-                a.HasMany(p => p.SubTechnologies);
+                a.HasMany(p => p.Technologies);
             });
 
             ProgrammingLanguage[] programmingLanguageEntitySeeds =
                 { new(1, "C#"), new(2, "Java"), new(3, "Python"), new(4, "JavaScript") };
             modelBuilder.Entity<ProgrammingLanguage>().HasData(programmingLanguageEntitySeeds);
 
-            modelBuilder.Entity<SubTechnology>(a =>
+            modelBuilder.Entity<Technology>(a =>
             {
-                a.ToTable("SubTechnologies").HasKey(k => k.Id);
+                a.ToTable("Technologies").HasKey(k => k.Id);
 
                 a.Property(p => p.Id).HasColumnName("Id");
                 a.Property(p => p.ProgrammingLanguageId).HasColumnName("ProgrammingLanguageId");
@@ -47,11 +53,68 @@ namespace Persistence.Contexts
                 a.HasOne(p => p.ProgrammingLanguage);
             });
 
-            SubTechnology[] subTechnologyEntitySeeds =
+            Technology[] technologyEntitySeeds =
                 { new(1, 1, "WPF"), new(2, 1, "ASP.NET"), new(3, 2, "Spring"), new(4, 2, "JSP"), new(5, 4, "Vue"), new(6, 4, "React") };
-            modelBuilder.Entity<SubTechnology>().HasData(subTechnologyEntitySeeds);
+            modelBuilder.Entity<Technology>().HasData(technologyEntitySeeds);
 
+            modelBuilder.Entity<User>(a =>
+            {
+                a.ToTable("Users").HasKey(k => k.Id);
+
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.FirstName).HasColumnName("FirstName");
+                a.Property(p => p.LastName).HasColumnName("LastName");
+                a.Property(p => p.Email).HasColumnName("Email");
+                a.HasIndex(p => p.Email).IsUnique();
+                a.Property(p => p.PasswordHash).HasColumnName("PasswordHash");
+                a.Property(p => p.PasswordSalt).HasColumnName("PasswordSalt");
+                a.Property(p => p.Status).HasColumnName("Status");
+                a.Property(p => p.GitHubAddress).HasColumnName("GitHubAdress");
+
+                a.HasMany(p => p.UserOperationClaims);
+                a.HasMany(p => p.RefreshTokens);
+            });
+
+            modelBuilder.Entity<OperationClaim>(a =>
+            {
+                a.ToTable("OperationClaims").HasKey(k => k.Id);
+
+                a.Property(p => p.Name).HasColumnName("Name");
+
+                a.HasIndex(p => p.Name).IsUnique();
+            });
+
+            OperationClaim[] operationClaimSeeds = { new(1, ClaimRoles.admin.ToString()) };
+            modelBuilder.Entity<OperationClaim>().HasData(operationClaimSeeds);
+
+            modelBuilder.Entity<UserOperationClaim>(a =>
+            {
+                a.ToTable("UserOperationClaims").HasKey(k => k.Id);
+
+                a.Property(p => p.UserId).HasColumnName("UserId");
+                a.Property(p => p.OperationClaimId).HasColumnName("OperationClaimId");
+                a.HasIndex(p => new { p.UserId, p.OperationClaimId }).IsUnique();
+
+                a.HasOne(p => p.User);
+                a.HasOne(p => p.OperationClaim);
+            });
+
+            modelBuilder.Entity<RefreshToken>(a =>
+            {
+                a.ToTable("RefreshTokens").HasKey(k => k.Id);
+
+                a.Property(p => p.UserId).HasColumnName("UserId");
+                a.Property(p => p.Token).HasColumnName("Token");
+                a.Property(p => p.Expires).HasColumnName("Expires");
+                a.Property(p => p.Created).HasColumnName("Created");
+                a.Property(p => p.CreatedByIp).HasColumnName("CreatedByIp");
+                a.Property(p => p.Revoked).HasColumnName("Revoked");
+                a.Property(p => p.RevokedByIp).HasColumnName("RevokedByIp");
+                a.Property(p => p.ReplacedByToken).HasColumnName("ReplacedByToken");
+                a.Property(p => p.ReasonRevoked).HasColumnName("ReasonRevoked");
+
+                a.HasOne(p => p.User);
+            });
         }
-
     }
 }
